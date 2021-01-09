5a8df69f2494567e1ffc6503063c3e1e53cf80bb
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-10 01:40:36 +0300

    Add task 1-Вычислить факториал from module 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1){
+    return 1;
+  }
+  let result = 1;
+  for (let i = n; i >= 1; i--){
+    result *= i;
+  }
+  return result;
 }
