d40c88524714674b82848d7ad0cdd00f89f79a3d
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-14 01:53:22 +0300

    Rafactoring 1-module-4-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,16 +4,13 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  function checkSpam(str){
-    let lowStr = str.toLowerCase();
+  let lowStr = str.toLowerCase();
 
-    if (str.length === 0){
-      return false;
-    } else if (lowStr.includes("1xbet") || (lowStr.includes("xxx"))) {
-      return true;
-    } else {
-      return false;
-    }
+  if (str.length === 0) {
+    return false;
+  } else if (lowStr.includes("1xbet") || (lowStr.includes("xxx"))) {
+    return true;
+  } else {
+    return false;
   }
-
 }
