99fbf56ce0d07b3972898b833939870461e47d52
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-14 04:20:23 +0300

    Add function truncate

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,11 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+  let result;
+  if (str.length <= maxlength){
+    return str;
+  } else {
+    result = str.substr(0, 19) + "…";
+    return result;
+  }
 }
