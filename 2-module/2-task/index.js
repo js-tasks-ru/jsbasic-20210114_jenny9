8f2791f6201327dddb009724d0c62206b09174af
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-22 23:51:58 +0300

    Add function isEmpty and 3-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,12 +4,8 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
-<<<<<<< HEAD
   for (let key in obj) {
     return false;
   }
   return true;
-=======
-  // ваш код...
->>>>>>> 32ed362659402992fd9d41171f11faf70f583b40
 }
