7f92d053732ee9f6c96895541a4c6563b04fe280
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-26 15:02:43 +0300

    Add function camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,19 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  if (!str) {return str;}
+
+  const result = [];
+  const arr = str.split("-");
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (i > 0) {
+      result.push(item[0].toUpperCase() + item.slice(1));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result.join("");
 }
