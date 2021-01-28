b7a49c7290b2bceabfc7dc9125d571accb7e3628
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-01-28 23:50:24 +0300

    Refactoring function camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,19 +3,23 @@
  * @returns {string}
  */
 function camelize(str) {
-  if (!str) {return str;}
+  // if (!str) {
+  //   return str;
+  // }
+  // const result = [];
+  // const arr = str.split("-");
+  // for (let i = 0; i < arr.length; i++) {
+  //   const item = arr[i];
+  //   if (i > 0) {
+  //     result.push(item[0].toUpperCase() + item.slice(1));
+  //   } else {
+  //     result.push(item);
+  //   }
+  // }
+  // return result.join("");
 
-  const result = [];
-  const arr = str.split("-");
-
-  for (let i = 0; i < arr.length; i++) {
-    const item = arr[i];
-    if (i > 0) {
-      result.push(item[0].toUpperCase() + item.slice(1));
-    } else {
-      result.push(item);
-    }
-  }
-
-  return result.join("");
+  return str
+    .split("-")
+    .map((item, index) => !index ? item : item[0].toUpperCase() + item.slice(1))
+    .join("");
 }
