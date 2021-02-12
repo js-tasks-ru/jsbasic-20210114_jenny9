4ce6678e2f434379c2e89e33be51cce75e51d27b
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-02-13 00:49:49 +0300

    Add task 3 function highlight for module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,32 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    const ageCell = row.cells[1];
+    const genderCell = row.cells[2];
+    const statusCell = row.cells[3];
+
+    if (ageCell.innerText <= 18) {
+      row.style.textDecoration = "line-through";
+    }
+
+    if (genderCell.innerText === "m") {
+      row.classList.add("male");
+    }
+    if (genderCell.innerText === "f") {
+      row.classList.add("female");
+    }
+
+    if (statusCell.hasAttribute("data-available")) {
+      if (statusCell.getAttribute("data-available") === "true") {
+        row.classList.add("available");
+      } else {
+        row.classList.add("unavailable");
+      }
+    } else {
+      row.setAttribute("hidden");
+    }
+  }
 }
+
