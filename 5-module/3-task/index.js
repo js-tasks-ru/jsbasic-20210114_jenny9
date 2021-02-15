bc1e3a6beef9f57f9b9891c2bc88e37977d71c15
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-02-15 03:42:28 +0300

    Add function initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,36 @@
 function initCarousel() {
-  // ваш код...
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+  const inner = document.querySelector('.carousel__inner');
+  const slideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  let currentSlide = 0;
+
+  arrowLeft.addEventListener("click", onArrowLeftClick);
+  arrowRight.addEventListener("click", onArrowRightClick);
+  disableButtons();
+
+  function onArrowLeftClick() {
+    currentSlide--;
+    inner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+    disableButtons();
+  }
+
+  function onArrowRightClick() {
+    currentSlide++;
+    inner.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+    disableButtons();
+  }
+
+  function disableButtons() {
+    if (currentSlide === 0) {
+      arrowLeft.style.display = 'none';
+    } else {
+      arrowLeft.style.display = '';
+    }
+    if (currentSlide === 3) {
+      arrowRight.style.display = 'none';
+    } else {
+      arrowRight.style.display = '';
+    }
+  }
 }
